3166bf0d0d8b01503456355e1f17bab013a39637
adam <athornton@gmail.com>
2017-07-06 08:41:17 -0700

    Version typo.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ DESCRIPTION = 'JupyterHub authenticator that uses GH orgs as whitelists'
 AUTHOR = 'Adam Thornton'
 AUTHOR_EMAIL = 'athornton@lsst.org'
 URL = 'https://github.com/lsst-sqre/ghowlauth'
-VERSION = '0.10.0'
+VERSION = '0.0.10'
 LICENSE = 'MIT'
 
 
